d165806335f33583d8415fc67b87a5fd5076144e
1gnaci0 <otrosnombresfrancisco@gmail.com>
2023-03-13 21:11:11 -0300

    correccion Ejercicio 6

--- Ejercicios_extra_6.java
+++ Ejercicios_extra_6.java
@@ -4,39 +4,53 @@
  * and open the template in the editor.
  */
 package java_ejercicios_extras_1al10;
+
 import java.util.Scanner;
+
 /**
  *
  * @author Usuario
  */
 public class Ejercicios_extra_6 {
+    public static Scanner leer = new Scanner(System.in);
+
     public static void main(String[] args) {
-       
-     Scanner leer = new Scanner(System.in);
+
         System.out.println("Cantidad de personas:");
         
         int per = leer.nextInt();
-        
-        double [] v = new double [per];
-        
+
+        double[] v = new double[per];
+
         for (int i = 0; i < per; i++) {
             System.out.println("Altura:");
             double alt = leer.nextDouble();
-            
-            v[i]=alt;
-   
+            v[i] = alt;
         }
-        int contotal = 0;
-        int contmenos = 0;
-        for (int i = 0; i < v.length; i++) {
-            if (v[i]<= 1.60){
-        contmenos = contmenos + 1;
-        }
-            contotal = contotal + 1;
-        }
-    
-        System.out.println("hay: "+contmenos+" cantidad de personas menores a 1.60."+"En total son: "+contotal+" personas.");
+       
+        double contMenos = 0;
+        int total_Menos = 0;
         
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] < 1.60) {
+                contMenos = contMenos + v[i];
+                total_Menos += 1;
+            }
+        }
+        
+        double totMenos = contMenos / total_Menos;
+        
+        double total = 0;
+        double prom = 0;
+        
+        for (int i = 0; i < v.length; i++) {
+            prom = prom + v[i];
+        }
+        
+        total = prom / per;
+
+        System.out.println("el promedio de estatura menor a 1.60 es: " + totMenos  + " El promedio total de estaturas es: " + total);
+
     }
-    
+
 }
